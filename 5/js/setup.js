a1bd1c33e825b5d6207a0044849941986c5b0df2
Keks <keks@htmlacademy.ru>
2019-06-24 13:00:52 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/setup.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+var wizardList = document.querySelector('.setup-similar-list');
+
+// Функция генерации случайных данных
+var getRandom = function (randoms) {
+  var randomValue = randoms[Math.floor(Math.random() * randoms.length)];
+  return randomValue;
+};
+
+var generateRandom = function (names, lastNames, mantleСolors, pupilColors) {
+  var items = [];
+
+  for (var i = 0; i < 4; i++) {
+    items[i] = {
+      name: '',
+      coatColor: '',
+      eyesColor: ''
+    };
+
+    items[i].name = getRandom(names) + ' ' + getRandom(lastNames);
+    items[i].coatColor = getRandom(mantleСolors);
+    items[i].eyesColor = getRandom(pupilColors);
+  }
+
+  return items;
+};
+
+var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+// Функция создания персонажа
+var createWizard = function (objectWizard) {
+  var newWizard = wizardTemplate.cloneNode(true);
+  newWizard.querySelector('.setup-similar-label').textContent = objectWizard.name;
+  newWizard.querySelector('.wizard-coat').style.fill = objectWizard.coatColor;
+  newWizard.querySelector('.wizard-eyes').style.fill = objectWizard.eyesColor;
+
+  return newWizard;
+};
+
+//  Функция добавления персонажей в разметку
+var addWizard = function (wizardElements) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < wizardElements.length; i++) {
+    var newWizardElement = createWizard(wizardElements[i]);
+    fragment.appendChild(newWizardElement);
+  }
+
+  wizardList.appendChild(fragment);
+};
+
+// Генерируем массив данных и добавляем персонажей
+var randomData = generateRandom(WIZARD_NAMES, WIZARD_LAST_NAMES, WIZARD_COAT_COLORS, WIZARD_EYES_COLORS);
+addWizard(randomData);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
