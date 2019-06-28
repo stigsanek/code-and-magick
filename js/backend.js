577566e7e880c40a1c36f386aef5aadf0d7f5ba2
stigsanek <stigsanek@yandex.ru>
2019-06-28 23:48:09 +0400

    Заменяет моки на загрузку данных с сервера

---
+++ js/backend.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Модуль работы с сестью
+(function () {
+  var URL = 'https://js.dump.academy/code-and-magick/data';
+  var STATUS_CODE_SUCCESS = 200;
+  var ONE_SECOND = 1000;
+
+  var loadData = function (onLoad, onError, method) {
+
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === STATUS_CODE_SUCCESS) {
+        onLoad(xhr.response, method);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout / ONE_SECOND + 'секунд');
+    });
+
+    xhr.timeout = ONE_SECOND;
+
+    xhr.open('GET', URL);
+    xhr.send();
+  };
+
+  window.backend = {
+    load: loadData
+  };
+})();
--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 'use strict';
 
 (function () {
-  var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var WIZARD_FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
@@ -17,12 +15,11 @@
   var eyesInputElement = userDialog.querySelector('#eyes-color');
   var fireballInputElement = userDialog.querySelector('#fireball-color');
 
-
   document.addEventListener('DOMContentLoaded', function () {
-    var data = window.data.get(WIZARD_NAMES, WIZARD_LAST_NAMES, WIZARD_COAT_COLORS, WIZARD_EYES_COLORS);
-    window.dialog.add(data, window.wizard.create);
+    // Загружает данные
+    window.backend.load(window.dialog.add, onError, window.wizard.create);
     userDialog.querySelector('.setup-similar').classList.remove('hidden');
-
+    // Управляет внешним видом персонажа
     window.colorize.change(coatWizardElement, coatInputElement, WIZARD_COAT_COLORS, 'fill');
     window.colorize.change(eyesWizardElement, eyesInputElement, WIZARD_EYES_COLORS, 'fill');
     window.colorize.change(fireballWizardElement, fireballInputElement, WIZARD_FIREBALL_COLORS, 'background-color');
