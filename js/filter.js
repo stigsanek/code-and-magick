e310cc603377d682323cce0c0f123c6b2748cea4
stigsanek <stigsanek@yandex.ru>
2019-07-04 12:32:57 +0400

    Обновляет время интервала

--- js/filter.js
+++ js/filter.js
@@ -5,7 +5,7 @@
   var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var WIZARD_FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-  var DEBOUNCE_INTERVAL = 300;
+  var DEBOUNCE_INTERVAL = 500;
 
   var userDialog = document.querySelector('.setup');
 
