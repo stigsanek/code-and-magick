55e67c57437f524d33e10e22c69adc646f62f816
stigsanek <stigsanek@yandex.ru>
2019-07-04 20:31:29 +0400

    Исправляет ошибки линтера

--- js/wizard.js
+++ js/wizard.js
@@ -16,17 +16,17 @@
     return newWizard;
   };
 
-   // Метод добавления персонажей в разметку
+  // Метод добавления персонажей в разметку
   var wizardList = document.querySelector('.setup-similar-list');
 
   var addWizard = function (data) {
-      if (data.length > WIZARD_LENGTH) {
-        data.length = WIZARD_LENGTH;
-        wizardList.innerHTML = '';
-        for (var i = 0; i < WIZARD_LENGTH; i++) {
-          wizardList.appendChild(createWizard(data[i]));
-        }
+    if (data.length > WIZARD_LENGTH) {
+      data.length = WIZARD_LENGTH;
+      wizardList.innerHTML = '';
+      for (var i = 0; i < WIZARD_LENGTH; i++) {
+        wizardList.appendChild(createWizard(data[i]));
       }
+    }
   };
 
   window.wizard = {
